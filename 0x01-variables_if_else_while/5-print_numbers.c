5f16819f99f9527a72f06d08b6ac7552bb541f18
JennVP <becjenif@gmail.com>
2020-09-17 21:01:49 +0000

    new

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char n;
+for (n = 0; n < 10; n + 1)
+putchar (n);
+putchar ('\n');
+return (0);
+}
