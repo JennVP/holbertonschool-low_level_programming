e1593cfa528a9aa756a5e7365b9fbdc5372e4496
JennVP <becjenif@gmail.com>
2020-09-18 03:22:17 +0000

    new

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,9 +9,9 @@ int c;
 c = 0;
 while (c < 100)
 {
-/*remove the last digit and recur*/
+/*remove the last digit and print the first digit from 0 to 99*/
 putchar((c / 10) + '0');
-/*print last digit*/
+/*print last digit until 99*/
 putchar((c % 10) + '0');
 if (c < 99)
 {
