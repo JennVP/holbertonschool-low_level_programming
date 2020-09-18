d4492a37219374e514d77829505f71ec62bd01b1
JennVP <becjenif@gmail.com>
2020-09-18 19:02:47 +0000

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,18 +6,20 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i < 10; i++)
+for (i = 0; i <= 9; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = 0; j <= 9; j++)
 {
 if (j > i)
 {
 putchar(i + '0');
 putchar(j + '0');
+if (i < 8)
+{
 putchar(' ');
 putchar(',');
 }
-
+}
 }
 }
 putchar('\n');
