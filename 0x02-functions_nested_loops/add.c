6e2e693bad78c64651715b94e7a9351cfb041b6e
JennVP <becjenif@gmail.com>
2020-09-19 19:11:54 +0000

    new

---
+++ 0x02-functions_nested_loops/add.c
@@ -0,0 +1,14 @@
+#include"holberton.h"
+/**
+ * add - returns the sum of two integers
+ * @a: Integer
+ * @b: Integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b);
+{
+int r;
+r = (a + b);
+return (r);
+}
