3d31c42e9532ef0fa004bc8c564a29a746129961
JennVP <becjenif@gmail.com>
2020-09-24 16:42:44 +0000

    new

---
+++ 0x04-more_functions_nested_loops/.#printm.c
@@ -0,0 +1,1 @@
+root@vagrant-ubuntu-trusty-64.2510:1600956631
---
+++ 0x04-more_functions_nested_loops/printm.c
@@ -0,0 +1,11 @@
+#include "bootcamp.h"
+
+/**
+ * print_most_numbers - prints digits 0 - 9, skipping 2 & 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+
+}
