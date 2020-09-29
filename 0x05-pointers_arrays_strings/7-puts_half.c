21496e55d5f72d4e956a417c8ca4235431ad0296
JennVP <becjenif@gmail.com>
2020-09-29 08:52:08 +0000

    new

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include"holberton.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int len = 0, half = 0;
+while (*(str + len))
+len++;
+if (len % 2 == 0)
+half = len / 2;
+else
+half = (len + 1) / 2;
+while (*(str + half))
+_putchar(*(str + half++));
+_putchar('\n');
+}
